734b656cafe6eafab66dc1aa19034b74543b2c02
Simone <simonescaccia.s@gmail.com>
2023-05-25 17:16:49 +0200

    Fixing hx711 drivers

--- riot-code/main.c
+++ riot-code/main.c
@@ -7,11 +7,13 @@
 const gpio_t gpio_dout = GPIO_PIN(0 ,1);
 const gpio_t gpio_sck = GPIO_PIN(0, 2);
 const uint16_t divider = 1;
-const uint8_t read_times = 200;
+const uint8_t read_times = 1;
+const hx711_gain_t gain = CHANNEL_A_128;
 
 const hx711_params_t hx711_params = {
     gpio_sck,
     gpio_dout,
+    gain,
     read_times,
     divider
 };
@@ -20,17 +22,28 @@ static hx711_t dev;
 
 int main(void)
 {
+
     xtimer_sleep(3);
 
-    puts("Hello World!");
-
+    puts("HX711 test application\n");
+    puts("+------------Initializing------------+");
     hx711_init(&dev, &hx711_params);
+    hx711_power_up(&dev);
+    puts("Initialization successful\n\n");
+
+    puts("+--------Starting Measurements--------+");
+    int32_t value_before = hx711_get_units(&dev);
+    printf("value before taring: %"PRIu32"\n", value_before);
+    hx711_tare(&dev);
+    int32_t value_after = hx711_get_units(&dev);
+    printf("value after taring: %"PRIu32"\n", value_after);
 
     while(1) {
-        printf("Value: %d\n", hx711_get_units(&dev, read_times));
+        value_after = hx711_get_units(&dev);
+        printf("value after taring: %"PRIu32"\n", value_after);
 
         xtimer_sleep(1);
     }
-
+    
     return 0;
 }
--- riot-code/modules/driver_hx711.c
+++ riot-code/modules/driver_hx711.c
@@ -1,12 +1,43 @@
+/*
+ * Copyright (C) 2018 - 2020 Philipp-Alexander Blum <philipp-blum@jakiku.de>
+ *               2019 Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     drivers_sensors
+ * @brief       Driver for the HX711 weigh scale ADC
+ *
+ * @{
+ * @file
+ * @brief       HX711 driver
+ *
+ * @author      Philipp-Alexander Blum <philipp-blum@jakiku.de>
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * @}
+ */
+
 #include "driver_hx711.h"
 #include "periph/gpio_util.h"
 #include <stdio.h>
 
-static int i = 0;
-
-static int32_t hx711_read(hx711_t *dev)
+/**
+ * @brief Needs to wait for device to be ready while initialization
+ */
+static int _hx711_is_ready(hx711_t *dev)
 {
-   union {
+    return gpio_read(dev->params.dout) == 0;
+}
+
+int32_t hx711_read(hx711_t *dev)
+{
+    while (!_hx711_is_ready(dev)) {}
+
+    union {
         int32_t i32;
         struct {
             uint8_t data[3];
@@ -19,36 +50,37 @@ static int32_t hx711_read(hx711_t *dev)
     gpio_t sck = dev->params.sck;
     gpio_t dout = dev->params.dout;
 
+    unsigned gain = dev->params.gain;
+
     value.u8.data[2] = gpio_util_shiftin(dout, sck);
     value.u8.data[1] = gpio_util_shiftin(dout, sck);
     value.u8.data[0] = gpio_util_shiftin(dout, sck);
 
+    for (unsigned i = 0; i < gain; i++) {
+        gpio_set(sck);
+        gpio_clear(sck);
+    }
+
     if (value.u8.data[2] & 0x80) {
         value.u8.fill_byte = 0xFF;
-    } else {
-        value.u8.fill_byte = 0x00;
-    }
-
-    value.i32 = (value.u8.fill_byte << 24) | (value.u8.data[2] << 16) | (value.u8.data[1] << 8) | value.u8.data[0];
-
-    if (i == 0) {
-        printf("fill_byte: %"PRIu8"", value.u8.fill_byte);
-        printf("data[2]: %"PRIu8"", value.u8.data[2]);
-        printf("data[1]: %"PRIu8"", value.u8.data[1]);
-        printf("data[0]: %"PRIu8"", value.u8.data[0]);
-        printf("i32: %"PRIu32"", value.i32);
-        i++;
-    }
-
+    }   
+    
     return value.i32;
 }
 
-static void hx711_power_up(hx711_t *dev) 
+static void _hx711_set_gain(hx711_t *dev, hx711_gain_t gain)
 {
+    dev->params.gain = gain;
     gpio_clear(dev->params.sck);
+    hx711_read(dev);
 }
 
-void hx711_init (hx711_t *dev, const hx711_params_t *params) 
+static void _hx711_set_offset(hx711_t *dev, int32_t offset)
+{
+    dev->offset = offset;
+}
+
+void hx711_init(hx711_t *dev, const hx711_params_t *params)
 {
     dev->params = *params;
     dev->offset = 0;
@@ -56,27 +88,46 @@ void hx711_init (hx711_t *dev, const hx711_params_t *params)
     gpio_init(params->sck, GPIO_OUT);
     gpio_init(params->dout, GPIO_IN);
 
-    hx711_power_up(dev);
+    _hx711_set_gain(dev, params->gain);
 }
 
-static int32_t hx711_read_average(hx711_t *dev, uint8_t times)
+int32_t hx711_read_average(hx711_t *dev)
 {
     int32_t sum = 0;
-    uint8_t i = times;
+    uint8_t i = dev->params.read_times;
 
     while (i--) {
         sum += hx711_read(dev);
     }
 
-    return sum / times;
+    return sum / dev->params.read_times;
 }
 
-static int32_t hx711_get_value(hx711_t *dev, uint8_t times)
+int32_t hx711_get_value(hx711_t *dev)
 {
-    return hx711_read_average(dev, times) - dev->offset;
+    return hx711_read_average(dev) - dev->offset;
 }
 
-int32_t hx711_get_units(hx711_t *dev, int8_t times)
+
+int32_t hx711_get_units(hx711_t *dev)
 {
-    return hx711_get_value(dev, times) / dev->params.divider;
+    return hx711_get_value(dev) / dev->params.divider;
+}
+
+void hx711_tare(hx711_t *dev)
+{
+    int32_t sum = hx711_read_average(dev);
+
+    _hx711_set_offset(dev, sum);
+}
+
+void hx711_power_down(hx711_t *dev)
+{
+    gpio_clear(dev->params.sck);
+    gpio_set(dev->params.sck);
+}
+
+void hx711_power_up(hx711_t *dev)
+{
+    gpio_clear(dev->params.sck);
 }
